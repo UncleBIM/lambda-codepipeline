8617fdbf65a2432d94d0444690a54e36f9d05bdf
Jakachai Saekor <jakachai.saekor@gmail.com>
2019-09-06 17:39:38 +0700

    fix

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func findAll() (events.APIGatewayProxyResponse, error) {
 		TableName: aws.String(os.Getenv("TABLE_NAME")),
 	})
 
-	ctx, cancel := context.WithTimeout(context.Background, 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	res, err := req.Send(ctx)
